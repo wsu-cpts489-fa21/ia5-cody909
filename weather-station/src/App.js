7662309586fb70d04068fa6c6853da6a7322389f
Cody Mercadante <cody.mercadante@wsu.edu>
2021-10-13 16:59:45 -0700

    added sign in page

--- weather-station/src/App.js
+++ weather-station/src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
 import WeatherObservations from "./WeatherObservations";
+import Login from "./Login";
 import { useState, useCallback } from "react";
 //import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const wrapperSetIsLoggedIn = useCallback(
+    (val) => {
+      setIsLoggedIn(val);
+    },
+    [setIsLoggedIn]
+  );
+
+  const signout = () => {
+    setIsLoggedIn(false);
+  }
 
   return (
     <div className="App">
+      {isLoggedIn ? (
+        <div>
         <WeatherObservations />
+        <button 
+          className="signout"
+          onClick={signout}
+          >
+            sign out</button>
+        </div>
+      ) : (
+        <Login
+          isLoggedIn={isLoggedIn}
+          isLoggedInSetter={wrapperSetIsLoggedIn}
+        />
+      )}
     </div>
   );
 }
